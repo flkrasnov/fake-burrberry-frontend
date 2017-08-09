26b701849ff046cf3da96cee2d90a50735cda9cc
Dima Krasnov <fl.krasnov@gmail.com>
2017-08-09 22:23:34 +0300

    feat: Helmet added, styled-components added. Header component refactored, SC added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import React, { Component } from 'react';
-import Header from './components/header';
-import Main from './components/main-section/main-section';
-import Footer from './components/footer'
-import './App.css';
-
+import React, { Component } from "react";
+import Helmet from "react-helmet";
+import Header from "./components/header";
+import Main from "./components/main-section/main-section";
+import Footer from "./components/footer";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-
       <div>
+        <Helmet
+          htmlAttributes={{ lang: "en" }}
+          title="Long Cotton Gabardine Car Coat | Men - Burberry"
+          meta={[
+            {
+              name: "viewport",
+              content: "width=device-width, initial-scale=1.0"
+            },
+            { name: "description", content: "Welcome to burberry.com" },
+            { "http-equiv": "X-UA-Compatible", content: "IE=Edge" },
+            { charset: "UTF-8" }
+          ]}
+          link={[
+            { rel: "shortcut icon", href: "%PUBLIC_URL%/favicon.ico" },
+            {
+              rel: "stylesheet",
+              href: "https://fonts.googleapis.com/css?family=Lora:400,400i&amp;subset=cyrillic"
+            },
+            {
+              rel: "stylesheet",
+              href: "https://fonts.googleapis.com/css?family=Raleway:400,500,600,700"
+            },
+            { rel: "manifest", href: "%PUBLIC_URL%/manifest.json" }
+          ]}
+        />
         <Header />
         <Main />
         <Footer />
       </div>
-      
     );
   }
 }
-
 
 export default App;
--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 import logo from "../logo.svg";
+import styled from "styled-components";
+
+const Header = styled.header`
+  background: #fff;
+  border-bottom: 1px solid #c6c6c6;
+`;
+
+const Ancor = styled.a`
+  .page-header-logo {
+    display: block;
+    margin: 1rem auto;
+  }
+
+  @media screen and (min-width: 48rem) {
+    .page-header-logo {
+      margin-top: 1.5rem;
+      margin-bottom: 1.5rem;
+    }
+  }
+`;
 
 export default () => {
   return (
-    <header className="page-header">
-      <a href="">
+    <Header>
+      <Ancor href="/">
         <img className="page-header-logo" src={logo} alt="Burberry logo" />
-      </a>
-    </header>
+      </Ancor>
+    </Header>
   );
 };
