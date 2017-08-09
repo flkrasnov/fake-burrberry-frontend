ebe9a95ef36370358fac9025efb93f734016bf10
Dima Krasnov <fl.krasnov@gmail.com>
2017-08-10 01:09:03 +0300

    feat: links component refactored

--- src/components/main-section/links/links.js
+++ src/components/main-section/links/links.js
@@ -1,22 +1,58 @@
-import React, { Component } from 'react';
+import React from "react";
+import styled from "styled-components";
+
+const Block = styled.section`
+  padding: 0 .5rem;
+  margin-bottom: 4rem;
+
+  @media screen and (min-width: 48rem) {
+    display: none;
+  }
+`;
+
+const Heading = styled.h2`
+  display: inline-block;
+  padding-bottom: .5rem;
+  margin-top: 1rem;
+  margin-bottom: 1.5rem;
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: 1.25rem;
+  border-bottom: 1px solid #111;
+  text-transform: uppercase;
+`;
+
+const LinksWrap = styled.div`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+`;
+
+const Link = styled.a`
+  display: block;
+  margin-bottom: 1rem;
+  font-family: Lora, serif;
+  font-size: .875rem;
+  line-height: 1.0625rem;
+  text-decoration: none;
+  font-style: italic;
+  color: #171717;
+`;
 
 export default () => {
   return (
-
-    <section className="more-links">
+    <Block>
       <div className="container">
-        <h2 className="more-links-heading">more for you</h2>
+        <Heading>more for you</Heading>
       </div>
 
-
       <div className="container">
-        <div className="more-links-block">
-          <a className="more-links-link" href="">Men’s Black Trench Coats</a>
-          <a className="more-links-link" href="">Men’s Short Trench Coats</a>
-          <a className="more-links-link" href="">Men’s Long Trench Coats</a>
-        </div>
+        <LinksWrap>
+          <Link href="/">Men’s Black Trench Coats</Link>
+          <Link href="/">Men’s Short Trench Coats</Link>
+          <Link href="/">Men’s Long Trench Coats</Link>
+        </LinksWrap>
       </div>
-    </section>
-
+    </Block>
   );
 };
