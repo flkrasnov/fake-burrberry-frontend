ea1b908117df8b73701bdcd42a92e37cd7d0e362
Dima Krasnov <fl.krasnov@gmail.com>
2017-08-10 01:42:50 +0300

    feat: recomendations components refactored

--- src/components/main-section/recomendations/recomendations.js
+++ src/components/main-section/recomendations/recomendations.js
@@ -1,81 +1,123 @@
-import React, { Component } from "react";
+import React from "react";
+import styled from "styled-components";
+
+const Recomendations = styled.section`
+  padding: 0 .5rem;
+
+  @media screen and (min-width: 48rem) {
+    padding-left: 0;
+    padding-right: 0;
+    border-bottom: 0;
+  }
+`;
+
+const Heading = styled.h2`
+  display: inline-block;
+  padding-bottom: .5rem;
+  margin-top: 3rem;
+  margin-bottom: 1.5rem;
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: 1.1875rem;
+  border-bottom: 1px solid #111;
+  text-transform: uppercase;
+
+  @media screen and (min-width: 48rem) {
+    margin-top: 0;
+    margin-bottom: 1rem;
+    line-height: 1.1875rem;
+  }
+`;
+
+const BlocksWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-flow: row wrap;
+
+  @media screen and (min-width: 48rem) {
+    margin-bottom: 4rem;
+  }
+`;
+
+const AncorBlock = styled.a`
+  box-sizing: border-box;
+  display: block;
+  flex-basis: 50%;
+  margin-bottom: 2rem;
+  color: #171717;
+  text-decoration: none;
+`;
+
+const BlockImg = styled.img`
+  display: block;
+  width: 100%;
+  height: 100%;
+`;
+
+const BlockName = styled.h3`
+  margin: .5rem 0;
+  font-family: Lora, serif;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.25rem;
+
+  @media screen and (min-width: 48rem) {
+    line-height: 1.2;
+  }
+`;
+
+const BlockPrice = styled.h5`
+  margin: 0;
+  font-size: .75rem;
+  line-height: .875rem;
+  font-weight: 400;
+  color: #999;
+`;
 
 export default () => {
   return (
-    <section className="recomendations">
+    <Recomendations>
       <div className="container">
-        <h2 className="recomendations-heading">we recommend</h2>
+        <Heading>we recommend</Heading>
       </div>
 
-      <div className="recomendations-blocks">
+      <BlocksWrapper>
         <div className="container">
           <div className="row">
             <div className="col-xs-6 col-sm-3">
-              <a className="recomendations-blocks-block" href="">
-                <img
-                  className="recomendations-blocks-block-img"
-                  src="img/wr1.jpg"
-                  alt="Emroided Hooded"
-                />
-                <h3 className="recomendations-blocks-block-name">
-                  Emroided Hooded Content For Three Lines
-                </h3>
-                <h5 className="recomendations-blocks-block-price">
-                  27 000 руб
-                </h5>
-              </a>
+              <AncorBlock href="">
+                <BlockImg src="img/wr1.jpg" alt="Emroided Hooded" />
+                <BlockName>Emroided Hooded Content For Three Lines</BlockName>
+                <BlockPrice>27 000 руб</BlockPrice>
+              </AncorBlock>
             </div>
 
             <div className="col-xs-6 col-sm-3">
-              <a className="recomendations-blocks-block" href="">
-                <img
-                  className="recomendations-blocks-block-img"
-                  src="img/wr2.jpg"
-                  alt="Relaxed Fit Stretch Jeans"
-                />
-                <h3 className="recomendations-blocks-block-name">
-                  Relaxed Fit Stretch Jeans Content For Three Lines
-                </h3>
-                <h5 className="recomendations-blocks-block-price">
-                  22 500 руб
-                </h5>
-              </a>
+              <AncorBlock href="">
+                <BlockImg src="img/wr2.jpg" alt="Relaxed Fit Stretch Jeans" />
+                <BlockName>Relaxed Fit Stretch Jeans Content For Three Lines</BlockName>
+                <BlockPrice>22 500 руб</BlockPrice>
+              </AncorBlock>
             </div>
 
             <div className="col-xs-6 col-sm-3">
-              <a className="recomendations-blocks-block" href="">
-                <img
-                  className="recomendations-blocks-block-img"
-                  src="img/wr3.jpg"
-                  alt="Leather and House"
-                />
-                <h3 className="recomendations-blocks-block-name">
-                  Leather and House Check Content For Three Lines
-                </h3>
-                <h5 className="recomendations-blocks-block-price">
-                  120 000 руб
-                </h5>
-              </a>
+              <AncorBlock href="">
+                <BlockImg src="img/wr3.jpg" alt="Leather and House" />
+                <BlockName>Leather and House Check Content For Three Lines</BlockName>
+                <BlockPrice>120 000 руб</BlockPrice>
+              </AncorBlock>
             </div>
 
             <div className="col-xs-6 col-sm-3">
-              <a className="recomendations-blocks-block" href="">
-                <img
-                  className="recomendations-blocks-block-img"
-                  src="img/wr4.jpg"
-                  alt="Leather Wingtip"
-                />
-                <h3 className="recomendations-blocks-block-name">
-                  Leather Wingtip Check Content For Three Lines
-                </h3>
-                <h5 className="recomendations-blocks-block-price">
-                  46 000 руб
-                </h5>
-              </a>
+              <AncorBlock href="">
+                <BlockImg src="img/wr4.jpg" alt="Leather Wingtip" />
+                <BlockName>Leather Wingtip Check Content For Three Lines</BlockName>
+                <BlockPrice>46 000 руб</BlockPrice>
+              </AncorBlock>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </BlocksWrapper>
+    </Recomendations>
   );
 };
